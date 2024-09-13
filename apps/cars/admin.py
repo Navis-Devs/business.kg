e4a06ebe773ba42e36720cfae19d817b1de1758a
NurbekT7 <sh7nurbek@gmail.com>
2024-09-13 23:38:57 +0600

    database structure

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.cars.models import CarType, CarMark
 
-# Register your models here.
+admin.site.register(CarType)
+admin.site.register(CarMark)
--- apps/cars/models.py
+++ apps/cars/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CarType(models.Model):
+    id_car_type = models.IntegerField(
+        _("Id")
+    )
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+
+class CarMark(models.Model):
+    id_car_mark = models.IntegerField(
+        _("iId")
+    )
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.RESTRICT
+    )
+    name_rus = models.CharField(
+        _("Name rus"),
+        max_length=255
+    )
