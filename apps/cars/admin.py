6e84eb34112563298aa206bac4a89e43eea96d09
NurbekT7 <sh7nurbek@gmail.com>
2024-09-14 17:13:19 +0600

    car model

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.cars.models import CarType, CarMark
+from apps.cars.models import *
 
 admin.site.register(CarType)
 admin.site.register(CarMark)
+admin.site.register(CarModel)
--- apps/cars/models.py
+++ apps/cars/models.py
@@ -3,17 +3,39 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CarType(models.Model):
-    id_car_type = models.IntegerField(
-        _("Id")
-    )
     name = models.CharField(
         _("Name"),
         max_length=255
     )
 
+    def __str__(self):
+        return self.name
+
+
 class CarMark(models.Model):
-    id_car_mark = models.IntegerField(
-        _("iId")
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.RESTRICT
+    )
+    name_rus = models.CharField(
+        _("Name rus"),
+        max_length=255
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    id_car_mark = models.ForeignKey(
+        "CarMark",
+        verbose_name=_("Id car mark"),
+        on_delete=models.RESTRICT
     )
     name = models.CharField(
         _("Name"),
