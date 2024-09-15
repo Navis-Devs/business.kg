dfc6b7f38c824432e1e15563dbe2408fc0f3171d
NurbekT7 <sh7nurbek@gmail.com>
2024-09-15 18:45:02 +0600

    database structure | Models

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import SET_NULL
 from django.utils.translation import gettext_lazy as _
 
 
@@ -49,4 +50,220 @@ class CarModel(models.Model):
     name_rus = models.CharField(
         _("Name rus"),
         max_length=255
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarGeneration(models.Model):
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_model = models.ForeignKey(
+        "CarModel",
+        verbose_name=_("Id car model"),
+        on_delete=models.RESTRICT
+    )
+    year_begin = models.CharField(
+        _("Year begin"),
+        max_length=255
+    )
+    year_end = models.CharField(
+        _("Year end"),
+        max_length=255
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarSerie(models.Model):
+    id_car_model = models.ForeignKey(
+        "CarModel",
+        verbose_name=_("Id car model"),
+        on_delete=SET_NULL,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_generation = models.ForeignKey(
+        "CarGeneration",
+        verbose_name=_("Id car generation"),
+        on_delete=models.RESTRICT
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarModification(models.Model):
+    id_car_serie = models.ForeignKey(
+        "CarSerie",
+        verbose_name=_("Id car serie"),
+        on_delete=models.RESTRICT
+    )
+    id_car_model = models.ForeignKey(
+        "CarModel",
+        verbose_name=_("Id car model"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+# Branch 1, From CarModification to CarCharacteristicValue
+
+class CarCharacteristic(models.Model):
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_parent = models.ForeignKey(
+        "self",
+        verbose_name=_("Id parent"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+
+class CarCharacteristicValue(models.Model):
+    value = models.CharField(
+        _("Value"),
+        max_length=255
+    )
+    unit = models.CharField(
+        _("Unit"),
+        max_length=255
+    )
+    id_car_characteristic = models.ForeignKey(
+        "CarCharacteristic",
+        verbose_name=_("Id car characteristic"),
+        on_delete=models.RESTRICT,
+    )
+    id_car_modification = models.ForeignKey(
+        "CarModification",
+        verbose_name=_("Car modification"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+
+# branch 2, from CarModification to CarEquipment
+
+class CarEquipment(models.Model):
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_car_modification = models.ForeignKey(
+        "CarModification",
+        verbose_name=_("Id car modification"),
+        on_delete=models.RESTRICT
+    )
+    price_min = models.IntegerField(
+        _("Price min")
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=SET_NULL,
+        null=True,
+        blank=True
+    )
+    year = models.IntegerField(
+        _("year")
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarOption(models.Model):
+    name = models.CharField(
+        _("Name"),
+        max_length=255
+    )
+    id_parent = models.ForeignKey(
+        "self",
+        verbose_name=_("Id parent"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+class CarOptionValue(models.Model):
+    is_base = models.PositiveSmallIntegerField(
+        _("Is base")
+    )
+    id_car_option = models.ForeignKey(
+        "CarOption",
+        verbose_name=_("Id car option"),
+        on_delete=models.RESTRICT
+    )
+    id_car_equipment = models.ForeignKey(
+        "CarEquipment",
+        verbose_name=_("Id car equipment"),
+        on_delete=models.RESTRICT
+    )
+    id_car_type = models.ForeignKey(
+        "CarType",
+        verbose_name=_("Id car type"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
     )
